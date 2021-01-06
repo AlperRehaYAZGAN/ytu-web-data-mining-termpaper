9ba6f91e64577a64b1c6ee19b4f2cee8a721d336
AlperRehaYAZGAN <43668181+AlperRehaYAZGAN@users.noreply.github.com>
2021-01-06 22:15:29 +0300

    github initial commit

--- app.py
+++ app.py
@@ -2,50 +2,57 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
+# NLP Start
 
-    # For debugging
-    print(f"got name {name}")
+from rake_nltk import Rake
 
-    response = {}
+r = Rake() # Uses stopwords for english from NLTK, and all puntuation characters.
 
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+# r.extract_keywords_from_text("Hello from Alper")
+# r.get_ranked_phrases() # To get keyword phrases ranked highest to lowest.
 
-    # Return the response in json format
-    return jsonify(response)
+# Uses stopwords for english from NLTK, and all puntuation characters.
+# NLP ENDS
 
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
+import trafilatura
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
 
 # A welcome message to test our server
 @app.route('/')
 def index():
     return "<h3>Web Data Mining Project</h3><br> <h4>18011020 - Alper Reha YAZGAN</h4><br> <h4>Web site will be available when project is ready.</h4>"
 
-if __name__ == '__main__':
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+
+
+@app.route('/from/text', methods=['POST'])
+def analysis_from_text():
+    # Retrieve the name from url parameter
+    response = {}
+    response["ERROR"] = "no name found, please send a name."
+    return jsonify(response)
+
+
+@app.route('/from/url', methods=['GET'])
+def analysis_from_url_get():
+    # param = request.form.get('text')
+    # print(param)
+    downloaded = trafilatura.fetch_url('http://adrien.barbaresi.eu/blog/trafilatura-main-text-content-python.html')
+    result = trafilatura.extract(downloaded)
+    text3="NLTK, bir veri setinin ön işlemesini(preprocessing), yani veriyi makinenin anlayacağı hale getireceğimiz zaman bizi gereksiz kelimelerle uğraştırmaktan da kurtarıyor. Aslında gereksiz demek biraz yanlış oluyor, sadece öznitelik çıkarımı yaparken önemli olmayan kelimelerle(ben, sen, de, da, ki, ile vs) uğraştırmaktan kurtarıyor. Gelin NLTK’nın bize sağladığı bu güzelliği de deneyelim, Türkçe içinde destekliyor hem"
+    r.extract_keywords_from_text(text3)    
+    return jsonify(result)
+
+
+@app.route('/from/url', methods=['POST'])
+def analysis_from_url():
+    # param = request.form.get('text')
+    # print(param)
+    text1 = "Avengers: Infinity War was a 2018 American superhero film based on the Marvel Comics superhero team the Avengers. It is the 19th film in the Marvel Cinematic Universe (MCU). The running time of the movie was 149 minutes and the box office collection was around 2 billion dollars. (Source: Wikipedia)"
+    r.extract_keywords_from_text(text1)
+    
+    r.get_ranked_phrases() # To get keyword phrases ranked highest to lowest.
+    return "Succes"
+
